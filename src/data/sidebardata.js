dc0c383a70c6c8bc1c3f7a5acaa1c8527b4260b1
Sanchit Hajela <sanchit10hajela@gmail.com>
2024-03-10 22:39:47 +0530

    modularised sidebar

---
+++ src/data/sidebardata.js
@@ -0,0 +1,41 @@
+const sidebarItems = [
+  {
+    id: 1,
+    label: "Home",
+    icon: "/icons/icon-1.svg",
+    active: true,
+  },
+  {
+    id: 2,
+    label: "Notifications",
+    icon: "/icons/icon-2.svg",
+    active: false,
+  },
+  {
+    id: 3,
+    label: "",
+    icon: "/icons/icon-3.svg",
+    active: false,
+  },
+  {
+    id: 4,
+    label: "",
+    icon: "/icons/icon-4.svg",
+    active: false,
+  },
+  {
+    id: 5,
+    label: "",
+    icon: "/icons/icon-5.svg",
+    active: false,
+  },
+  {
+    id: 6,
+    label: "",
+    icon: "/icons/icon-6.svg",
+    active: false,
+  },
+  // Add more items as needed
+];
+
+export default sidebarItems;
